225129da266df312f256039e68356c3a374f31c0
Alex-Rosano-SAE <60865530+Alex-Rosano-SAE@users.noreply.github.com>
2020-03-30 11:22:49 +1100

    new project files
    this is project 2 gaming programming

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The BattlesSystem handles the organisation of rounds, selecting the dancers to dance off from each side.
+/// It then hands off to the fightManager to determine the outcome of 2 dancers dance off'ing.
+/// 
+/// TODO:
+///     Needs to hand the request for a dance off battle round by selecting a dancer from each side and 
+///         handing off to the fight manager, via GameEvents.RequestFight
+///     Needs to handle GameEvents.OnFightComplete so that a new round can start
+///     Needs to handle a team winning or loosing
+///     This may be where characters are set as selected when they are in a dance off and when they leave the dance off
+/// </summary>
+public class BattleSystem : MonoBehaviour
+{
+    public DanceTeam TeamA,TeamB;
+
+    public float battlePrepTime = 2;
+    public float fightWinTime = 2;
+
+    private void OnEnable()
+    {
+        GameEvents.OnRequestFighters += RoundRequested;
+        GameEvents.OnFightComplete += FightOver;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnRequestFighters -= RoundRequested;
+        GameEvents.OnFightComplete -= FightOver;
+    }
+
+    void RoundRequested()
+    {
+        StartCoroutine(DoRound());
+    }
+
+    void FightOver(FightResultData data)
+    {
+        //check for winner
+        if (data.outcome != 0)
+        {
+            data.winner.myTeam.EnableWinEffects();
+
+            data.defeated.myTeam.RemoveFromActive(data.defeated);
+        }
+
+        //if none
+        StartCoroutine(HandleFightOver());
+    }
+    
+    IEnumerator DoRound()
+    {
+        //Tell fight manager to attack? Or just listen for outcomes?
+        yield return new WaitForSeconds(battlePrepTime);
+
+        if(TeamA.activeDancers.Count > 0 && TeamB.activeDancers.Count > 0)
+        {
+            Character a = TeamA.activeDancers[Random.Range(0, TeamA.activeDancers.Count)];
+            Character b = TeamB.activeDancers[Random.Range(0, TeamB.activeDancers.Count)];
+            GameEvents.RequestFight(new FightEventData(a, b));
+        }
+        else
+        {
+            var winner = TeamA.activeDancers.Count > 0 ? TeamA : TeamB;
+            GameEvents.BattleFinished(winner);
+
+            winner.EnableWinEffects();
+
+            //log it battlelog also
+            Debug.Log("Game Over");
+        }
+    }
+
+    IEnumerator HandleFightOver()
+    {
+        yield return new WaitForSeconds(fightWinTime);
+        TeamA.DisableWinEffects();
+        TeamB.DisableWinEffects();
+        GameEvents.RequestFighters();
+    }
+}
---
+++ Assets/Scripts/CharacterNameGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Data object that can generate a number of dancer names
+/// 
+/// TODO:
+///     Needs to generate the number of requested <b>unique</b> character names for the dancers in our battle
+/// </summary>
+//[CreateAssetMenu(menuName = "Battle Objects/Character Name Generator")]
+//[System.Serializable]
+public class CharacterNameGenerator : ScriptableObject
+{
+    [Header("Possible first names")]
+    [SerializeField]
+    public List<string> firstNames;
+    [Header("Possible last names")]
+    [SerializeField]
+    public List<string> lastNames;
+    [Header("Possible nicknames")]
+    [SerializeField]
+    public List<string> nicknames;
+    [Header("Possible adjectives to describe the character")]
+    [SerializeField]
+    public List<string> descriptors;
+
+    public CharacterName[] GenerateNames(int namesNeeded)
+    {
+        CharacterName[] names = new CharacterName[namesNeeded];
+
+        for (int i = 0; i < namesNeeded; i++)
+        {
+            string first = firstNames[Random.Range(0, firstNames.Count)];
+            string last = lastNames[Random.Range(0, lastNames.Count)];
+            last.Remove;
+            object p = Debug.Log(last);
+            string nick = nicknames[Random.Range(0, nicknames.Count)];
+            string des = descriptors[Random.Range(0, descriptors.Count)];
+
+            names[i] = new CharacterName(first, last, nick, des);
+        }
+
+        return names;
+    }
+}
